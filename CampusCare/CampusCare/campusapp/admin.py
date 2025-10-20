52afefafa119eb58ab9a8037a148b944cd7d8918
aunjoleenusrath <aunjoleenusrath@gmail.com>
2025-10-21 02:27:41 +0600

    Add files via upload

--- CampusCare/CampusCare/campusapp/admin.py
+++ CampusCare/CampusCare/campusapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import MedicalService, MentalSupport, HealthMonitor, StudentSupport
 
-# Register your models here.
+
+@admin.register(MedicalService)
+class MedicalServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(MentalSupport)
+class MentalSupportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(HealthMonitor)
+class HealthMonitorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(StudentSupport)
+class StudentSupportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
