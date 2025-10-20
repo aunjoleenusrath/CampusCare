58b55396209de65d081bc95cc118f96d897b1ae0
aunjoleenusrath <aunjoleenusrath@gmail.com>
2025-10-21 02:20:11 +0600

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MedicalService
-
-class MedicalServiceForm(forms.ModelForm):
-    class Meta:
-        model = MedicalService
-        fields = ['name', 'description']
