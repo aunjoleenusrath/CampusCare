ed51ca34533008a4baf55eeab8a458e28d7284e3
aunjoleenusrath <aunjoleenusrath@gmail.com>
2025-10-21 02:46:15 +0600

    Add files via upload

--- CampusCare/CampusCare/urls.py
+++ CampusCare/CampusCare/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for CampusCare project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# home/urls.py
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # ✅ matches your view
+    path('login/', views.login_page, name='login_page'),
+    path('signup/', views.signup_page, name='signup_page'),
+    path('diseases/', views.diseases_page, name='diseases_page'),
+    path('payment/', views.payment_page, name='payment_page'),
+    path('gateway/', views.payment_gateway, name='payment_gateway'),
 ]
+
+
