ffdc1429a82bd431d089da8e474defa162222f9b
aunjoleenusrath <aunjoleenusrath@gmail.com>
2025-10-21 20:19:57 +0600

    Add files via upload

--- CampusCare/CampusCare/models.py
+++ CampusCare/CampusCare/models.py
@@ -1,32 +1,7 @@
 from django.db import models
 
-class MedicalService(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+# Create your models here.
 
-    def __str__(self):
-        return self.name
-
-
-class MentalSupport(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class HealthMonitor(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class StudentSupport(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
+class HomeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'home'
