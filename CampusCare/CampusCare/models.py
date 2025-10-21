2ab65f9d19f99adb19a52c24c8033bd1c760a5c9
aunjoleenusrath <aunjoleenusrath@gmail.com>
2025-10-21 20:01:16 +0600

    models of campusapp

--- CampusCare/CampusCare/models.py
+++ CampusCare/CampusCare/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 
+class MedicalService(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MentalSupport(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class HealthMonitor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class StudentSupport(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
