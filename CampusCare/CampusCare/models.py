1b43eb1a3f591135c31ad604070013a3bf3a0ba0
Dalila-Alam <23101157@uap-bd.edu>
2025-10-21 16:31:29 +0600

    Add files via upload

--- CampusCare/CampusCare/models.py
+++ CampusCare/CampusCare/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class HealthMonitor(models.Model]]):
+class HealthMonitor(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
