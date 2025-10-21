691a452542899bee54ec97a10fbe044fc9e01132
Dalila-Alam <23101157@uap-bd.edu>
2025-10-21 15:32:17 +0600

    Add files via upload

--- CampusCare/CampusCare/apps.py
+++ CampusCare/CampusCare/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
+class HealthmonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'healthmonitor'
