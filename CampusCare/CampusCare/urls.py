5b97b2517eb4f206d18a6f9222f50f335dbea186
Dalila-Alam <23101157@uap-bd.edu>
2025-10-21 16:32:38 +0600

    Add files via upload

--- CampusCare/CampusCare/urls.py
+++ CampusCare/CampusCare/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_monitors, name='list_monitorss'),
+    path('', views.list_monitors, name='list_monitors'),
     path('add/', views.add_monitor, name='add_monitor'),
     path('update/<int:id>/', views.update_monitor, name='update_monitor'),
     path('delete/<int:id>/', views.delete_monitor, name='delete_monitor'),
